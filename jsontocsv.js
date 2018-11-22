c9685ef8382242b65b02c5d9bcb15c7cbb677ae3
Albaobab <alban.moizan@etu.univ-nantes.fr>
2018-11-22 14:44:30 +0100

    json to csv

---
+++ jsontocsv.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const fs = require("fs");
+
+function jsonToCsv(token, megaCsvPath) {
+    fs.readFile("./data/tokenjson/" + token + '.json', null, (err, content) => {
+        if (err) throw err;
+        let tabJson = JSON.parse(content);
+
+        let string = token;
+        for(let column=1; column<=88; column++){
+            if(tabJson[column] != null){
+                for(let lign in tabJson[column]){
+                    string += ";"+JSON.stringify(tabJson[column][lign]);
+                }
+            }else string += ";";
+
+        }
+        string = string.replace(/\"[it]\":/g, '');
+        fs.appendFile(megaCsvPath, string, (err) =>{
+            if (err) throw err;
+            console.log('JSON mis dans le grand csv');
+        });
+    });
+}
+
+jsonToCsv("test", "data/megafile.csv");
