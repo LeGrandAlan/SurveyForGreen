a674e6dbd1b36a839dcfeb5f501dd4c6199be0f3
etienne912 <etienne9912@gmail.com>
2018-11-22 08:05:05 +0100

    update creation token

--- serveur.js
+++ serveur.js
@@ -31,9 +31,21 @@ let server = http.createServer(function (req, res) { // On reçoit la demande de
                 } else {
                     let html = data2;
                     fs.readFile('./data/questionsv2_min.json', null, (err, data) => {
-                        try {
-                            fs.readFile("./data/tokenjson/" + params['token'] + '.json', null, (err, content) => {
-                                let toEncode = "\n<script>var token = \`" + params['token'] + "\`;var json = \`" + data + "\`; var jsonrep = \`" + content + "\`;</script>\n";
+                        if (params['token']) {
+                            try {
+                                fs.readFile("./data/tokenjson/" + params['token'] + '.json', null, (err, content) => {
+                                    if (err) throw err;
+                                    let toEncode = "\n<script>var token = \`" + params['token'] + "\`;var json = \`" + data + "\`; var jsonrep = \`" + content + "\`;</script>\n";
+                                    html += toEncode;
+                                    fs.readFile('./view/index.html', null, (err, data3) => {
+                                        html += data3;
+                                        zlib.gzip(html, function (_, result) {
+                                            res.end(result);
+                                        });
+                                    });
+                                });
+                            } catch (e) {
+                                let toEncode = "\n<script>var token = \`" + Math.random().toString(36).substring(2) + "\`;var json = \`" + data + "\`; var jsonrep = \`" + null + "\`;</script>\n";
                                 html += toEncode;
                                 fs.readFile('./view/index.html', null, (err, data3) => {
                                     html += data3;
@@ -41,8 +53,8 @@ let server = http.createServer(function (req, res) { // On reçoit la demande de
                                         res.end(result);
                                     });
                                 });
-                            });
-                        } catch (e) {
+                            }
+                        } else {
                             let toEncode = "\n<script>var token = \`" + Math.random().toString(36).substring(2) + "\`;var json = \`" + data + "\`; var jsonrep = \`" + null + "\`;</script>\n";
                             html += toEncode;
                             fs.readFile('./view/index.html', null, (err, data3) => {
@@ -51,6 +63,7 @@ let server = http.createServer(function (req, res) { // On reçoit la demande de
                                     res.end(result);
                                 });
                             });
+
                         }
 
 
