b10052432e6c6651264bda844ea1a912652bb01f
Albaobab <alban.moizan@etu.univ-nantes.fr>
2018-11-23 11:36:23 +0100

    Merge branch 'master' of /home/alban/WebstormProjects/SurveyForGreen with conflicts.

--- serveur.js
+++ serveur.js
@@ -47,6 +47,7 @@ let server = http.createServer(function (req, res) { // On reçoit la demande de
                     fs.writeFileSync("data/jsonanswer/" + (((JSON.parse(chunk.toString()))["token"])['i']) + ".json", chunk.toString(), "UTF-8");
                     if (((JSON.parse(chunk.toString()))["88"]) != null) {
                         jsontocsv.jsonToCsv((JSON.parse(chunk.toString()))["token"]['i'], "./data/megafile.csv");
+                        console.log("Je suis ici");
                     }
                     res.writeHead(202);
                     res.end();
