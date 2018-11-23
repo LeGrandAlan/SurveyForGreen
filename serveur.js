8cff85e48b3f3dd336897bcfeac986eef1802d5b
etienne912 <etienne9912@gmail.com>
2018-11-23 11:19:51 +0100

    correction de bugs

--- serveur.js
+++ serveur.js
@@ -7,13 +7,14 @@ const fs = require("fs");
 const zlib = require("zlib");
 const jsontocsv = require("./jsontocsv");
 
-let ipBloque = [];
+let ipBloque = []; // une liste d'ip que l'on vas bloquer
 
-let cacheData = "";
+let cacheData = ""; // variable contenant la page de base déjà encodéé en gzip
 
 // On créer notre serveur
 let server = http.createServer(function (req, res) { // On reçoit la demande de connexion du client
 
+    // condition pour bloquer les tentatives d'intrusions (assez basique)
     if (url.parse(req.url).href.includes(".php") || ipBloque.includes(req.connection.remoteAddress)) {
         console.log("Bloquage de requête ! :");
         let date = new Date().toLocaleTimeString("en-US", {
@@ -22,7 +23,9 @@ let server = http.createServer(function (req, res) { // On reçoit la demande de
             minute: "numeric"
         });
         console.log(date + " : " + req.connection.remoteAddress + " : " + url.parse(req.url).href);
-        ipBloque.push(req.connection.remoteAddress);
+        if (!ipBloque.includes(req.connection.remoteAddress)) { // si elle n'est pas dans la liste des ip bloquées on l'ajoute
+            ipBloque.push(req.connection.remoteAddress);
+        }
     } else {
         // ------------------ Parsing de l'url -----------------------------
         let page = url.parse(req.url).pathname; // On parse le nom dans l'url
@@ -33,9 +36,10 @@ let server = http.createServer(function (req, res) { // On reçoit la demande de
         });
         console.log(date + " : " + req.connection.remoteAddress + " : " + url.parse(req.url).href);
 
-        let params = querystring.parse(url.parse(req.url).query); // On récupère les paramètres dans l'url puis les valeurs
+        // On récupère les paramètres dans l'url puis les valeurs
+        let params = querystring.parse(url.parse(req.url).query);
 
-        // ------------------ Savegarde fichier -----------------------------
+        // ------------------ Savegarde fichier passé en post-----------------------------
         if (req.method === "POST") {
             let lejson = "";
             req.on("data", (chunk) => {
@@ -60,14 +64,14 @@ let server = http.createServer(function (req, res) { // On reçoit la demande de
 
                 // // ------------------ Création d'une page avec des infos contenu dans l'URL -----------------------------
 
-                if (params["download"] === "true") {
+                if (params["download"] === "true") { // si on veut télécharger le
 
-                    fs.readFile("./data/megafile.csv", null, (err, data) => {
+                    fs.readFile("./data/megafile.csv", null, (err, data) => { // on ouvre le fichier
                         if (err) {
                             res.writeHead(404);
                             res.end();
                         } else {
-                            zlib.gzip(data, function (_, result) {
+                            zlib.gzip(data, function (_, result) { // on encode l'evoie du fichier en gzip
                                 res.setHeader("Content-disposition", "attachment; filename=resultat.csv");
                                 res.writeHead(200, {"Content-Encoding": "gzip"});
                                 res.end(result);
@@ -76,7 +80,7 @@ let server = http.createServer(function (req, res) { // On reçoit la demande de
                     });
                 } else {
                     res.writeHead(200, {"Content-Type": "text/html", "Content-Encoding": "gzip"});
-                    if (typeof params["token"] === 'undefined' && cacheData !== "") {
+                    if (typeof params["token"] === 'undefined' && cacheData !== "") { // si on va ouvrir une page simple, on charge le cache
                         console.log("utilisation du cache !");
                         res.end(cacheData);
                     } else {
@@ -86,14 +90,14 @@ let server = http.createServer(function (req, res) { // On reçoit la demande de
                                     console.log(err);
                                 } else {
                                     let html = data2;
-                                    fs.readFile("./data/questionsv2_min.json", null, (err, data) => {
+                                    fs.readFile("./data/questionsv2_min.json", null, (err, data) => { // on ouvre notre fichier contenant toutes les questions
                                         if (params["token"] && fs.existsSync("./data/jsonanswer/" + params["token"] + ".json")) {
                                             fs.readFile("./data/jsonanswer/" + params["token"] + ".json", null, (err, content) => {
                                                 if (err) {
                                                     console.log(err);
                                                     let toEncode = "\n<script>var json = \`" + data + "\`; var jsonrep =  " + null + " ;</script>\n";
-                                                    html += toEncode;
-                                                } else {
+                                                    html += toEncode; // on passe le contenu des variables dans le fichier
+                                                } else { // s'il a un token valide on lui passe son avancement dans le questionnaire
                                                     let toEncode = "\n<script>var token = \`" + params['token'] + "\`;var json = \`" + data + "\`; var jsonrep = \`" + content + "\`;</script>\n";
                                                     html += toEncode;
                                                 }
@@ -104,7 +108,7 @@ let server = http.createServer(function (req, res) { // On reçoit la demande de
                                                     });
                                                 });
                                             });
-                                        } else {
+                                        } else { // si pas de token, on passe la page de base
                                             let toEncode = "\n<script>var json = \`" + data + "\`; var jsonrep =  " + null + " ;</script>\n";
                                             html += toEncode;
                                             fs.readFile("./view/index.html", null, (err, data3) => {
@@ -127,15 +131,24 @@ let server = http.createServer(function (req, res) { // On reçoit la demande de
 // "</script>\n";
 
 
-            } else if (page === '/favicon.ico') {
-                // res.writeHead(200, {'Content-Type': 'image/x-icon'});
+            } else if (page === '/favicon.ico') { // on n'a pas de favicon donc erreur 410
                 res.writeHead(410);
                 res.end();
-            } else {
-                // TODO : remettre
+            } else if (page === '/source.zip') { // si le jury cherche le fichier source.zip
+                fs.readFile("./source.zip", null, (err, data) => { // on l'ouvre
+                    if (err) { // si il y a une erreur on renvoie un code 404
+                        res.writeHead(404);
+                        res.end();
+                    } else {
+                        zlib.gzip(data, function (_, result) { // sinon on l'envoie au client encoder en gzip
+                            res.setHeader("Content-disposition", "attachment; filename=source.zip");
+                            res.writeHead(200, {"Content-Encoding": "gzip"});
+                            res.end(result);
+                        });
+                    }
+                });
+            } else { // pour toute autre page, erreur 404
                 res.writeHead(404, {"Content-Type": "text/plain"});
-                // res.writeHead(202, {"Content-Type": "text/html"});
-                // res.end("<img src='https://media0.giphy.com/media/ZXlDOOsfV0a8U/giphy.gif?cid=e1bb72ff5bf6a90b36514e32554350fe'> ");
             }
         }
     }
